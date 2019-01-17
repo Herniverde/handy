ce6e06b22ff2b7ce356da317573fbad92dbffbf2
Herniverde <45546788+Herniverde@users.noreply.github.com>
2019-01-17 12:58:03 -0500

    Update product.ts

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -14,12 +14,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'product.html',
 })
 export class ProductPage {
-
+ item:any = {name: '', price: '', category: '', description: ''};
+     
+  postData.append('item_name', this.item.name);
+      postData.append('item_quantity', this.item.quantity);
+      postData.append('item_description', this.item.description);
+      postData.append('item_price', this.item.price);
+      postData.append('location', this.item.location);
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductPage');
   }
-
+buy() {
+    this.autProvider.buy(this.item.name, this.item.quantity, this.item.description, this.item.price, this.item.location).then(success => {
+      if(success) {
+        this.navCtrl.setRoot('MenuPage');
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Invalid sell',
+          message: 'Please check your input',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    }
+cancel() {
+    this.autProvider.buy(this.item.name, this.item.quantity, this.item.description, this.item.price, this.item.location).then(success => {
+      if(success) {
+        this.navCtrl.setRoot('MenuPage');
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Invalid action',
+          message: 'Error',
+          buttons: ['OK']
+        });
+        alert.present();
+      }                                                                                                                              
 }
